cab771af15cd0242d142ad433797b3123ca3c01f
gabe.hopper <gabe.hopper@c06bfad2-088c-11de-b17c-0f1ef23b492c>
2009-06-02 20:57:24 +0000

    branch to develop maven poms.

---
+++ branches/enzian-0.0.0.20090601/plugins/com.jolira.enzian.admin/src/test/java/com/jolira/enzian/admin/internal/ExampleServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.jolira.enzian.admin.internal;
+
+import java.util.Arrays;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import com.jolira.enzian.admin.ExampleService;
+
+public class ExampleServiceImplTest extends TestCase
+{
+    public void testExampleServiceScramble()
+    {
+        ExampleService anExampleService = new ExampleServiceImpl();
+
+        String in = "This is a test of the text scrambling service";
+        String out = anExampleService.scramble( in );
+
+        char[] inChars = in.toCharArray();
+        char[] outChars = out.toCharArray();
+
+        Arrays.sort( inChars );
+        Arrays.sort( outChars );
+
+        Assert.assertEquals( "Uses same letters", new String( inChars ), new String( outChars ) );
+    }
+}
---
+++ branches/enzian-0.0.0.20090601/plugins/com.jolira.enzian.loader/src/main/java/com/jolira/enzian/loader/ExampleService.java
@@ -0,0 +1,12 @@
+package com.jolira.enzian.loader;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService {
+    /**
+     * @param text
+     * @return nothing important
+     */
+    String scramble(String text);
+}
