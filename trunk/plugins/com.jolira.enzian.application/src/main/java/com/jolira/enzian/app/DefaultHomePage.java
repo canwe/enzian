1dbbeaef3c88884e3d4578454e251e46cd75eed5
gabe.hopper <gabe.hopper@c06bfad2-088c-11de-b17c-0f1ef23b492c>
2009-06-16 17:10:23 +0000

    Other SVN client didn't add these files when module was renamed.

---
+++ trunk/plugins/com.jolira.enzian.application/src/main/java/com/jolira/enzian/app/DefaultHomePage.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jolira.enzian.app;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * @author Joachim Kainz
+ *
+ */
+public class DefaultHomePage extends WebPage {
+  // nothing yet
+}
